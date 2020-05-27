ba6648a5e3d2167c1659d77aa3aca83b565438d3
Frederico batista <fred@penumbra.design>
2020-05-27 12:29:30 -0300

    fuck it ill print it all

--- penumbra-vuupt.php
+++ penumbra-vuupt.php
@@ -108,6 +108,8 @@ function pnmbr_add_to_vuupt( $order_id ){
           $address = formatted_shipping_address($order);
         	$apikey 	= get_option('pnmbr_vuupt_api');
 
+          $order->add_order_note($name);
+
 					$service_id = get_post_meta( $order_id, 'service_id' )[0];
 					$customer_id = get_post_meta( $order_id, 'customer_id' )[0];
 
