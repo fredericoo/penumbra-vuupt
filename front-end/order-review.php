506bfb8bea65c06487779a3188af999e7bd0bee0
Frederico batista <fred@penumbra.design>
2020-06-02 19:13:38 -0300

    added css class to customise row.

--- front-end/order-review.php
+++ front-end/order-review.php
@@ -7,7 +7,7 @@ function pnmbr_vuupt_review() {
   global $woocommerce;
 
 ?>
-        <tr>
+        <tr class="delivery_when">
             <th><?php echo __('Data da entrega','penumbra') ?></th>
             <td><?php echo date_i18n( __('j \d\e F \d\e Y','penumbra'), strtotime(get_delivery_when($woocommerce->cart->get_cart(), current_time('Y-m-d')))) ?></td>
         </tr>
